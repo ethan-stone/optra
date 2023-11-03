0c024fdd80a960616a94975495096a6196646e79
ethan-stone <ethan.stone9352@gmail.com>
2023-11-03 09:26:12 -0400

    support many methods of sending token

--- packages/api/src/main.py
+++ packages/api/src/main.py
@@ -1,9 +1,10 @@
+import base64
 import datetime
 from enum import Enum
 from typing import Annotated
 
 import jwt
-from fastapi import Body, FastAPI
+from fastapi import Body, FastAPI, Form, Header, HTTPException
 from pydantic import BaseModel
 
 from . import db
@@ -23,17 +24,80 @@ class GrantTypeEnum(str, Enum):
 
 
 class OAuthTokenParams(BaseModel):
+    client_id: str | None
+    client_secret: str | None
+    grant_type: GrantTypeEnum | None
+
+
+class BasicAuthHeader(BaseModel):
     client_id: str
-    client_secret: str
-    grant_type: GrantTypeEnum
+    cliet_secret: str
+
+
+def parse_basic_auth_header(header: str) -> BasicAuthHeader | None:
+    """
+    Parse a basic auth header. If it is not a basic auth header, return None.
+    """
+    header_split = header.split(" ")
+
+    if len(header_split) != 2:
+        return None
+
+    base64_encoded = header_split[1]
+
+    base64_decoded = base64.b64decode(base64_encoded).decode("utf-8")
+
+    base64_decoded_split = base64_decoded.split(":")
+
+    if len(base64_decoded_split) != 2:
+        return None
+
+    return BasicAuthHeader(
+        client_id=base64_decoded_split[0], cliet_secret=base64_decoded_split[1]
+    )
 
 
 @app.post("/oauth/token")
-def oauth_token(body: Annotated[OAuthTokenParams, Body()]):
+def oauth_token(
+    client_id: Annotated[str, Form()] = None,
+    client_secret: Annotated[str, Form()] = None,
+    grant_type: Annotated[str, Form()] = None,
+    body: Annotated[OAuthTokenParams, Body()] = None,
+    authorization: Annotated[str, Header()] = None,
+):
+    if (
+        body is None
+        and (client_id is None or client_secret is None or grant_type is None)
+        and authorization is None
+    ):
+        raise HTTPException(status_code=400, detail="Invalid request")
+
+    client_id_parsed = client_id
+    client_secret_parsed = client_secret
+    grant_type_parsed = grant_type
+
+    if authorization is not None:
+        basic_auth_header = parse_basic_auth_header(authorization)
+
+        client_id_parsed = basic_auth_header.client_id
+        client_secret_parsed = basic_auth_header.cliet_secret
+
+    if body is not None:
+        client_id_parsed = body.client_id
+        client_secret_parsed = body.client_secret
+        grant_type_parsed = body.grant_type
+
+    if (
+        client_id_parsed is None
+        or client_secret_parsed is None
+        or grant_type_parsed is None
+    ):
+        raise HTTPException(status_code=400, detail="Invalid request")
+
     now = datetime.datetime.now()
 
     payload = JwtPayload(
-        sub=body.client_id, iat=now, exp=now + datetime.timedelta(days=1)
+        sub=client_id_parsed, iat=now, exp=now + datetime.timedelta(days=1)
     )
 
     token = jwt.encode(
