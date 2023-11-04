6b3f8bae1e05cd0f2e6507dea2ec10eaf05e6d88
ethan-stone <ethan.stone9352@gmail.com>
2023-11-04 16:48:04 -0400

    use raw request because of unexpected behavior with From and Body

--- packages/api/src/main.py
+++ packages/api/src/main.py
@@ -1,10 +1,10 @@
 import base64
 import datetime
 from enum import Enum
-from typing import Annotated, List
+from typing import List, Optional
 
 import jwt
-from fastapi import Body, FastAPI, Form, Header, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 from pydantic import BaseModel
 
 from . import db
@@ -24,14 +24,14 @@ class GrantTypeEnum(str, Enum):
 
 
 class OAuthTokenParams(BaseModel):
-    client_id: str | None
-    client_secret: str | None
-    grant_type: GrantTypeEnum | None
+    client_id: Optional[str]
+    client_secret: Optional[str]
+    grant_type: GrantTypeEnum
 
 
 class BasicAuthHeader(BaseModel):
     client_id: str
-    cliet_secret: str
+    client_secret: str
 
 
 def parse_basic_auth_header(header: str) -> BasicAuthHeader | None:
@@ -53,42 +53,60 @@ def parse_basic_auth_header(header: str) -> BasicAuthHeader | None:
         return None
 
     return BasicAuthHeader(
-        client_id=base64_decoded_split[0], cliet_secret=base64_decoded_split[1]
+        client_id=base64_decoded_split[0], client_secret=base64_decoded_split[1]
     )
 
 
 @app.post("/oauth/token")
-def oauth_token(
-    client_id: Annotated[str, Form()] = None,
-    client_secret: Annotated[str, Form()] = None,
-    grant_type: Annotated[str, Form()] = None,
-    body: Annotated[OAuthTokenParams, Body()] = None,
-    authorization: Annotated[str, Header()] = None,
+async def oauth_token(
+    request: Request,
 ):
     """
     Generate an access token for a client. This supports the client credentials flow
     with the client_id, client_secret, and grant_type parameters being sent in a combination of
     the request body, authorization header, and form data.
     """
-    client_ids: List[str | None] = list()
-    client_secrets: List[str | None] = list()
-    grant_types: List[str | None] = list()
+    content_type = request.headers.get("Content-Type")
+    print(content_type)
 
-    client_ids.append(client_id)
-    client_secrets.append(client_secret)
-    grant_types.append(grant_type)
+    client_ids: List[str | None] = []
+    client_secrets: List[str | None] = []
+    grant_types: List[str | None] = []
 
-    if authorization is not None:
-        basic_auth_header = parse_basic_auth_header(authorization)
+    if content_type == "application/x-www-form-urlencoded":
+        form = await request.form()
+
+        form_parsed = OAuthTokenParams(
+            client_id=form.get("client_id"),
+            client_secret=form.get("client_secret"),
+            grant_type=form.get("grant_type"),
+        )
+
+        client_ids.append(form_parsed.client_id)
+        client_secrets.append(form_parsed.client_secret)
+        grant_types.append(form_parsed.grant_type)
+
+    elif content_type == "application/json":
+        body = await request.json()
+
+        body_parsed = OAuthTokenParams(
+            client_id=body.get("client_id"),
+            client_secret=body.get("client_secret"),
+            grant_type=body.get("grant_type"),
+        )
+
+        client_ids.append(body_parsed.client_id)
+        client_secrets.append(body_parsed.client_secret)
+        grant_types.append(body_parsed.grant_type)
+
+    auth_header = request.headers.get("Authorization")
+
+    if auth_header is not None:
+        basic_auth_header = parse_basic_auth_header(auth_header)
 
         if basic_auth_header is not None:
             client_ids.append(basic_auth_header.client_id)
-            client_secrets.append(basic_auth_header.cliet_secret)
-
-    if body is not None:
-        client_ids.append(body.client_id)
-        client_secrets.append(body.client_secret)
-        grant_types.append(body.grant_type)
+            client_secrets.append(basic_auth_header.client_secret)
 
     client_id_parsed = next((v for v in client_ids if v is not None), None)
     client_secret_parsed = next((v for v in client_secrets if v is not None), None)
@@ -103,9 +121,7 @@ def oauth_token(
 
     now = datetime.datetime.now()
 
-    payload = JwtPayload(
-        sub=client_id_parsed, iat=now, exp=now + datetime.timedelta(days=1)
-    )
+    payload = JwtPayload(sub="", iat=now, exp=now + datetime.timedelta(days=1))
 
     token = jwt.encode(
         payload.model_dump(),
