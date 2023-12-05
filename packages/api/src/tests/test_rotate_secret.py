183290f2c85859d52e13c075eb00cf8bad555645
ethan-stone <ethan.stone9352@gmail.com>
2023-12-04 21:45:48 -0500

    add test to check that version is incremented

--- packages/api/src/tests/test_rotate_secret.py
+++ packages/api/src/tests/test_rotate_secret.py
@@ -44,9 +44,22 @@ def test_should_rotate_secret(setup: SetupResult):
         "Authorization": f"Bearer {token.access_token}",
     }
 
+    old_version = setup.basic_client_without_rate_limit.version
+
     response = client.post("/v1/clients.rotateSecret", json=data, headers=headers)
 
     response_json = response.json()
 
     assert response.status_code == 200
     assert response_json["client_id"] == setup.basic_client_without_rate_limit.id
+
+    updated_client = client.get(
+        "/v1/clients.getClient",
+        params={"client_id": setup.basic_client_without_rate_limit.id},
+        headers=headers,
+    )
+
+    updated_client_json = updated_client.json()
+
+    assert updated_client_json["id"] == setup.basic_client_without_rate_limit.id
+    assert updated_client_json["version"] == old_version + 1
