1636360ed5673791171108127587d0148f39a7e7
ethan-stone <ethan.stone9352@gmail.com>
2023-12-28 22:08:09 -0500

    add tests for remove api scope

---
+++ packages/worker/integration_tests/v1-remove-api-scope.test.ts
@@ -0,0 +1,136 @@
+import { describe, expect, it } from 'vitest';
+import { testEnvSchema } from './test-env-schema';
+import { getOAuthToken } from './helpers';
+
+const env = testEnvSchema.parse(process.env);
+
+describe('POST /v1/apis.removeScope', () => {
+	it('should respond with 400 BAD_REQUEST if invalid body', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.ROOT_CLIENT_ID, env.ROOT_CLIENT_SECRET);
+
+		const req = new Request(`${env.BASE_URL}/v1/apis.removeScope`, {
+			method: 'POST',
+			body: JSON.stringify({}), // missing fields
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+		const resJson = await res.json();
+
+		expect(res.status).toBe(400);
+		expect(resJson).toHaveProperty('reason');
+		expect((resJson as any).reason).toBe('BAD_REQUEST');
+		expect(resJson).toHaveProperty('message');
+	});
+
+	it('should respond with 404 NOT_FOUND if api does not exist', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.ROOT_CLIENT_ID, env.ROOT_CLIENT_SECRET);
+
+		const req = new Request(`${env.BASE_URL}/v1/apis.removeScope`, {
+			method: 'POST',
+			body: JSON.stringify({
+				apiId: 'non-existent-api-id',
+				scopeId: 'non-existent-scope-id',
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+		const resJson = await res.json();
+
+		expect(res.status).toBe(404);
+		expect(resJson).toHaveProperty('reason');
+		expect((resJson as any).reason).toBe('NOT_FOUND');
+		expect(resJson).toHaveProperty('message');
+	});
+
+	it('should respond with 404 NOT_FOUND if token does not have access to api', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.BASIC_CLIENT_ID, env.BASIC_CLIENT_SECRET);
+
+		const req = new Request(`${env.BASE_URL}/v1/apis.removeScope`, {
+			method: 'POST',
+			body: JSON.stringify({
+				apiId: env.API_ID,
+				scopeId: 'non-existent-scope-id',
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+		const resJson = await res.json();
+
+		expect(res.status).toBe(404);
+		expect(resJson).toHaveProperty('reason');
+		expect((resJson as any).reason).toBe('NOT_FOUND');
+		expect(resJson).toHaveProperty('message');
+	});
+
+	it('should respond with 200 OK if scope does not exist', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.ROOT_CLIENT_ID, env.ROOT_CLIENT_SECRET);
+
+		const req = new Request(`${env.BASE_URL}/v1/apis.removeScope`, {
+			method: 'POST',
+			body: JSON.stringify({
+				apiId: env.API_ID,
+				scopeId: 'does-not-exist',
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+
+		expect(res.status).toBe(200);
+	});
+
+	it('should respond with 200 OK if scope exists and is deleted', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.ROOT_CLIENT_ID, env.ROOT_CLIENT_SECRET);
+
+		const req = new Request(`${env.BASE_URL}/v1/apis.removeScope`, {
+			method: 'POST',
+			body: JSON.stringify({
+				apiId: env.API_ID,
+				scopeId: 'example-scope', // inside bootstrap the apis are created with a scope named 'example-scope'
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+
+		expect(res.status).toBe(200);
+	});
+
+	it('should respond with 200 OK if scope does not exist', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.ROOT_CLIENT_ID, env.ROOT_CLIENT_SECRET);
+
+		const req = new Request(`${env.BASE_URL}/v1/apis.removeScope`, {
+			method: 'POST',
+			body: JSON.stringify({
+				apiId: env.API_ID,
+				scopeId: 'does-not-exist',
+			}),
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+
+		expect(res.status).toBe(200);
+	});
+});
