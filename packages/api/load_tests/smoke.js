cd510526c256269d66fdebe9204a7840ae5ca8a1
ethan-stone <ethan.stone9352@gmail.com>
2024-01-24 16:04:23 -0500

    add smoke test

---
+++ packages/api/load_tests/smoke.js
@@ -0,0 +1,28 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export const options = {
+	vus: 5,
+	duration: '2m',
+	summaryTrendStats: ['avg', 'min', 'med', 'max', 'p(90)', 'p(95)', 'p(99)', 'p(99.99)', 'count'],
+};
+
+const accessToken = JSON.parse(open('./access-token.json'));
+
+export default function main() {
+	const res = http.post(
+		'http://localhost:8787/v1/tokens.verifyToken',
+		JSON.stringify({
+			token: accessToken.accessToken,
+		}),
+		{
+			headers: {
+				'Content-Type': 'application/json',
+			},
+		}
+	);
+	check(res, {
+		'status is 200': (r) => r.status === 200,
+	});
+	sleep(1);
+}
