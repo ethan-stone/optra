d5c0b35ecf1e9e25490c7eec32344d2064d1cd20
ethan-stone <ethan.stone9352@gmail.com>
2024-01-26 13:03:59 -0500

    tests for get api

---
+++ packages/api/integration_tests/v1-get-api.test.ts
@@ -0,0 +1,130 @@
+import { describe, it, expect, beforeAll } from 'vitest';
+import { testEnvSchema } from './test-env-schema';
+import { createApi, getOAuthToken } from './helpers';
+
+const env = testEnvSchema.parse(process.env);
+
+describe('POST /v1/apis.getApi', () => {
+	let apiId: string;
+
+	beforeAll(async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.ROOT_CLIENT_ID, env.ROOT_CLIENT_SECRET);
+
+		const api = await createApi(env.BASE_URL, token, {
+			algorithm: 'hsa256',
+		});
+
+		apiId = api.id;
+	});
+
+	it('should respond with 400 BAD_REQUEST if invalid query params', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.ROOT_CLIENT_ID, env.ROOT_CLIENT_SECRET);
+
+		// missing query string param
+		const req = new Request(`${env.BASE_URL}/v1/apis.getApi`, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+		const resJson = await res.json();
+
+		expect(res.status).toBe(400);
+		expect(resJson).toHaveProperty('reason');
+		expect(resJson).toHaveProperty('message');
+		expect((resJson as any).reason).toBe('BAD_REQUEST');
+	});
+
+	it('should respond with 404 NOT_FOUND if api does not exist because it actually does not exist', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.ROOT_CLIENT_ID, env.ROOT_CLIENT_SECRET);
+
+		const url = new URL(`${env.BASE_URL}/v1/apis.getApi`);
+		url.searchParams.set('apiId', 'does-not-exist');
+
+		const req = new Request(url, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+		const resJson = await res.json();
+
+		expect(res.status).toBe(404);
+		expect(resJson).toHaveProperty('reason');
+		expect(resJson).toHaveProperty('message');
+		expect((resJson as any).reason).toBe('NOT_FOUND');
+	});
+
+	it('should respond with 403 FORBIDDEN if not root client', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.BASIC_CLIENT_ID, env.BASIC_CLIENT_SECRET);
+
+		const url = new URL(`${env.BASE_URL}/v1/apis.getApi`);
+		url.searchParams.set('apiId', apiId);
+
+		const req = new Request(url, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+		const resJson = await res.json();
+
+		expect(res.status).toBe(403);
+		expect(resJson).toHaveProperty('reason');
+		expect(resJson).toHaveProperty('message');
+		expect((resJson as any).reason).toBe('FORBIDDEN');
+	});
+
+	it('should respond with 404 NOT_FOUND api does not belong to client making request', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.OTHER_ROOT_CLIENT_ID, env.OTHER_ROOT_CLIENT_SECRET);
+
+		const url = new URL(`${env.BASE_URL}/v1/apis.getApi`);
+		url.searchParams.set('apiId', apiId);
+
+		const req = new Request(url, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+		const resJson = await res.json();
+
+		expect(res.status).toBe(404);
+		expect(resJson).toHaveProperty('reason');
+		expect(resJson).toHaveProperty('message');
+		expect((resJson as any).reason).toBe('NOT_FOUND');
+	});
+
+	it('should respond with 200 OK with api', async () => {
+		const token = await getOAuthToken(env.BASE_URL, env.ROOT_CLIENT_ID, env.ROOT_CLIENT_SECRET);
+
+		const url = new URL(`${env.BASE_URL}/v1/apis.getApi`);
+		url.searchParams.set('apiId', apiId);
+
+		const req = new Request(url, {
+			method: 'GET',
+			headers: {
+				'Content-Type': 'application/json',
+				Authorization: `Bearer ${token}`,
+			},
+		});
+
+		const res = await fetch(req);
+		const resJson = await res.json();
+
+		expect(res.status).toBe(200);
+		expect((resJson as any).id).toEqual(apiId);
+	});
+});
