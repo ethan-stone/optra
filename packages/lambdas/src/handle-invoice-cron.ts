b4c34e14f06a78a1941a303f5eea660f6a135260
ethan-stone <ethan.stone9352@gmail.com>
2024-02-01 10:57:50 -0500

    send messages in batches

--- packages/lambdas/src/handle-invoice-cron.ts
+++ packages/lambdas/src/handle-invoice-cron.ts
@@ -1,10 +1,60 @@
 import { ScheduledHandler } from "aws-lambda";
 import { db } from "./db";
 import { sqsClient } from "./sqs";
-import { SendMessageCommand } from "@aws-sdk/client-sqs";
+import { SendMessageBatchCommand } from "@aws-sdk/client-sqs";
 import { InvoiceWorkspaceEvent } from "@optra/core/event-schemas";
 import { Config } from "sst/node/config";
-import { retry } from "./retry";
+
+function chunkArray<T>(array: T[], chunkSize: number): T[][] {
+  const chunks = [];
+
+  for (let i = 0; i < array.length; i += chunkSize) {
+    chunks.push(array.slice(i, i + chunkSize));
+  }
+  return chunks;
+}
+
+async function sendBatch(
+  messages: string[],
+  attempt: number
+): Promise<string[]> {
+  const entries = messages.map((message, index) => ({
+    Id: index.toString(),
+    MessageBody: message,
+  }));
+
+  try {
+    const command = new SendMessageBatchCommand({
+      Entries: entries,
+      QueueUrl: Config.MESSAGE_QUEUE_URL,
+    });
+
+    const res = await sqsClient.send(command);
+
+    const failed =
+      res.Failed?.map((failed) => messages[parseInt(failed.Id as string)]) ??
+      [];
+
+    if (failed.length > 0 && attempt < 3) {
+      console.log(`Retrying failed messages, attempt ${attempt + 1}`);
+      return await sendBatch(failed, attempt + 1);
+    } else {
+      return failed;
+    }
+  } catch (error) {
+    if (attempt < 3) {
+      console.log(
+        `Error sending batch, retrying attempt ${attempt + 1}`,
+        error
+      );
+      return await sendBatch(messages, attempt + 1);
+    } else {
+      console.error(`Failed to send batch of messages`, error);
+
+      return messages;
+    }
+  }
+}
 
 export const handler: ScheduledHandler = async (event) => {
   console.log("Queueing invoice generation for all workspaces");
@@ -21,9 +71,11 @@ export const handler: ScheduledHandler = async (event) => {
   const year = now.getUTCFullYear();
   const month = now.getUTCMonth() + 1;
 
-  for (const workspace of workspaces) {
-    console.log(`Queueing invoice for workspace ${workspace.workspaceId}`);
+  const messages: InvoiceWorkspaceEvent[] = [];
+  const messagesThatFailedSchemaValidation: Record<string, unknown>[] = [];
 
+  console.log(`Validating messages`);
+  for (const workspace of workspaces) {
     const msg: InvoiceWorkspaceEvent = {
       eventType: "workspace.invoice",
       payload: {
@@ -34,24 +86,45 @@ export const handler: ScheduledHandler = async (event) => {
       timestamp: Date.now(),
     };
 
-    try {
-      await retry({
-        callback: async () => {
-          const command = new SendMessageCommand({
-            MessageBody: JSON.stringify(InvoiceWorkspaceEvent.parse(msg)),
-            QueueUrl: Config.MESSAGE_QUEUE_URL,
-          });
+    const parseResult = InvoiceWorkspaceEvent.safeParse(msg);
 
-          await sqsClient.send(command);
-        },
-        maxRetries: 3,
-        retryDelay: 1000,
-      });
-    } catch (error) {
-      console.error(error);
-      console.error(
-        `Failed to queue invoice for workspace ${workspace.workspaceId}`
-      );
+    if (parseResult.success) {
+      messages.push(parseResult.data);
+    } else {
+      messagesThatFailedSchemaValidation.push(msg);
     }
   }
+
+  console.log(
+    `Queuing ${messages.length} invoice events and skipping ${messagesThatFailedSchemaValidation.length} events that failed schema validation`
+  );
+
+  const batches = chunkArray(messages, 10);
+
+  let failedMessages: string[] = [];
+
+  for (const batch of batches) {
+    const batchFailedMessages = await sendBatch(
+      batch.map((msg) => JSON.stringify(msg)),
+      0
+    );
+    failedMessages = failedMessages.concat(batchFailedMessages);
+  }
+
+  if (messagesThatFailedSchemaValidation.length > 0) {
+    console.error(
+      `Skipped ${messagesThatFailedSchemaValidation.length} messages that failed schema validation`
+    );
+    console.error(
+      `Failed schema validation:`,
+      messagesThatFailedSchemaValidation
+    );
+  }
+
+  if (failedMessages.length > 0) {
+    console.error(`Failed to send ${failedMessages.length} messages`);
+    console.error(`Failed messages:`, failedMessages);
+  }
+
+  console.log("Done queueing invoice generation for all workspaces");
 };
