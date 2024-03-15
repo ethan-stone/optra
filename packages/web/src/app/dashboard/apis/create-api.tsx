5b1519dd7df687beb49410d75ca49af1bec6e86a
ethan-stone <ethan.stone9352@gmail.com>
2024-03-14 22:29:36 -0400

    allow selection of algorithm

--- packages/web/src/app/dashboard/apis/create-api.tsx
+++ packages/web/src/app/dashboard/apis/create-api.tsx
@@ -3,6 +3,15 @@
 import { Button } from "@/components/ui/button";
 import { Dialog, DialogContent } from "@/components/ui/dialog";
 import { Input } from "@/components/ui/input";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { api } from "@/trpc/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -10,6 +19,7 @@ import { useState } from "react";
 export function CreateApi() {
   const [isOpen, setIsOpen] = useState(false);
   const [apiName, setApiName] = useState("");
+  const [algorithm, setAlgorithm] = useState("");
 
   const router = useRouter();
 
@@ -43,20 +53,33 @@ export function CreateApi() {
           {createApi.isLoading ? "Creating..." : "Create API"}
         </Button>
         <DialogContent>
-          <div className="mt-6 flex flex-col">
+          <div className="flex flex-col gap-4">
+            <h2 className="text-2xl font-semibold">Create API</h2>
             <Input
               type="text"
               value={apiName}
               placeholder="API Name"
               onChange={(e) => setApiName(e.target.value)}
             />
+            <Select onValueChange={(e) => setAlgorithm(e)}>
+              <SelectTrigger>
+                <SelectValue placeholder="Select a signing algorithm" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectLabel>Signing Algorithm</SelectLabel>
+                  <SelectItem value="rsa256">RSA256</SelectItem>
+                  <SelectItem value="hsa256">HSA256</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
             <Button
               className="mb-6 mt-2"
               disabled={createApi.isLoading}
               onClick={() => {
                 createApi.mutate({
                   name: apiName,
-                  algorithm: "rsa256",
+                  algorithm: algorithm as "rsa256" | "hsa256",
                 });
               }}
             >
