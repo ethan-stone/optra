3cf4e37528cfb847584646901332746b9e0695b4
ethan-stone <ethan.stone9352@gmail.com>
2024-05-26 00:00:27 -0400

    add permission form validation

--- packages/web/src/app/dashboard/apis/[apiId]/permissions/add-permission-form.tsx
+++ packages/web/src/app/dashboard/apis/[apiId]/permissions/add-permission-form.tsx
@@ -5,25 +5,35 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { api } from "@/trpc/react";
 import { useParams, useRouter } from "next/navigation";
-import { useState } from "react";
-import { z } from "zod";
+import { type SubmitHandler, useForm } from "react-hook-form";
 
-const schema = z.object({
-  name: z.string().min(3),
-  description: z.string().min(3).optional(),
-});
+type FormInput = {
+  name: string;
+  description: string;
+};
 
 export function AddPermissionForm() {
   const router = useRouter();
   const params = useParams<{ apiId: string }>();
 
-  const [name, setName] = useState("");
-  const [desc, setDesc] = useState("");
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    reset,
+  } = useForm<FormInput>({
+    defaultValues: {
+      name: "",
+      description: "",
+    },
+  });
 
   const addScope = api.apis.addScope.useMutation({
     onSuccess() {
-      setName("");
-      setDesc("");
+      reset({
+        name: "",
+        description: "",
+      });
       router.refresh();
     },
     onError(err) {
@@ -32,45 +42,41 @@ export function AddPermissionForm() {
     },
   });
 
+  const onSubmit: SubmitHandler<FormInput> = (data) => {
+    addScope.mutate({
+      apiId: params.apiId,
+      description: data.description,
+      name: data.name,
+    });
+  };
+
   return (
-    <div className="mb-2 flex flex-row gap-4">
+    <form
+      className="mb-2 flex flex-row gap-4"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <div className="flex flex-grow flex-col gap-1">
         Name
         <Input
+          {...register("name", { required: true, minLength: 1, maxLength: 30 })}
           placeholder="api:create"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
         />
+        {errors.name !== undefined && (
+          <p className="text-sm text-red-500">Name is required</p>
+        )}
       </div>
       <div className="flex flex-grow flex-col gap-1">
         Description
-        <Input
-          placeholder="Create APIs"
-          value={desc}
-          onChange={(e) => setDesc(e.target.value)}
-        />
+        <Input {...register("description")} placeholder="Create APIs" />
+        {errors.description !== undefined && (
+          <p className="text-sm text-red-500">Name is required</p>
+        )}
       </div>
       <div className="flex items-end">
-        <Button
-          className="flex flex-grow"
-          onClick={() => {
-            const valid = schema.safeParse({ name, description: desc });
-
-            if (!valid.success) {
-              alert("Name must be at least 3 characters long");
-              return;
-            }
-
-            addScope.mutate({
-              apiId: params.apiId,
-              name: name,
-              description: desc,
-            });
-          }}
-        >
+        <Button className="flex flex-grow" type="submit">
           {addScope.isLoading ? <Spinner /> : "Add"}
         </Button>
       </div>
-    </div>
+    </form>
   );
 }
