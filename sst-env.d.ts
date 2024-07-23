2725950508fa8ace546658c8de1f039f7f0cf17d
ethan-stone <ethan.stone9352@gmail.com>
2024-07-22 22:28:43 -0400

    update smoke test

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -3,54 +3,6 @@
 import "sst"
 declare module "sst" {
   export interface Resource {
-    AWSAccessKeyId: {
-      type: "sst.sst.Secret"
-      value: string
-    }
-    AWSSecretAccessKey: {
-      type: "sst.sst.Secret"
-      value: string
-    }
-    Api: {
-      type: "sst.aws.ApiGatewayV2"
-      url: string
-    }
-    DbUrl: {
-      type: "sst.sst.Secret"
-      value: string
-    }
-    JwksBucket: {
-      name: string
-      type: "sst.aws.Bucket"
-    }
-    MessageDLQ: {
-      type: "sst.aws.Queue"
-      url: string
-    }
-    MessageQueue: {
-      type: "sst.aws.Queue"
-      url: string
-    }
-    SchedulerFailedDLQ: {
-      type: "sst.aws.Queue"
-      url: string
-    }
-    TinyBirdApiKey: {
-      type: "sst.sst.Secret"
-      value: string
-    }
-    TinyBirdGenerationsEndpoint: {
-      type: "sst.sst.Secret"
-      value: string
-    }
-    TinyBirdUrl: {
-      type: "sst.sst.Secret"
-      value: string
-    }
-    TinyBirdVerificationsEndpoint: {
-      type: "sst.sst.Secret"
-      value: string
-    }
   }
 }
 export {}
