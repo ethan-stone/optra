6f27fdb33b867843f2697915158ed164e67770e1
ethan-stone <ethan.stone9352@gmail.com>
2024-08-26 21:06:45 -0400

    remove deployment

--- sst-env.d.ts
+++ sst-env.d.ts
@@ -1,28 +1,7 @@
 /* tslint:disable */
 /* eslint-disable */
-import "sst"
+import "sst";
 declare module "sst" {
-  export interface Resource {
-    DbUrl: {
-      type: "sst.sst.Secret"
-      value: string
-    }
-    MessageDLQ: {
-      type: "sst.aws.Queue"
-      url: string
-    }
-    MessageQueue: {
-      type: "sst.aws.Queue"
-      url: string
-    }
-    SchedulerFailedDLQ: {
-      type: "sst.aws.Queue"
-      url: string
-    }
-    StripeApiKey: {
-      type: "sst.sst.Secret"
-      value: string
-    }
-  }
+  export interface Resource {}
 }
-export {}
+export {};
