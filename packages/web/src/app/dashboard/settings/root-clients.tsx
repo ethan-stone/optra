34b520eee4f55e02cfc4ea12602569edafb65f27
ethan-stone <ethan.stone9352@gmail.com>
2024-10-15 15:42:24 -0400

    make look of root clients page consistent

--- packages/web/src/app/dashboard/settings/root-clients.tsx
+++ packages/web/src/app/dashboard/settings/root-clients.tsx
@@ -1,7 +1,16 @@
 "use client";
 
-import { Spinner } from "@/components/icons/spinner";
+import { useToast } from "@/components/hooks/use-toast";
+import { Ellipses } from "@/components/icons/ellipses";
 import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Separator } from "@/components/ui/separator";
 import { api } from "@/trpc/react";
 import { Copy } from "lucide-react";
@@ -14,6 +23,8 @@ type Props = {
 function RootClientItem(props: { id: string; name: string }) {
   const router = useRouter();
 
+  const { toast } = useToast();
+
   const deleteRootClient = api.clients.deleteRootClient.useMutation({
     onSuccess() {
       router.refresh();
@@ -25,31 +36,67 @@ function RootClientItem(props: { id: string; name: string }) {
   });
 
   return (
-    <div className="hover:bg-stone-100">
-      <div className="flex flex-row items-center justify-between space-y-1 px-4 py-5">
-        <h4 className="flex flex-row gap-2 font-medium leading-none">
-          {props.name}
-        </h4>
+    <div className="m-1 rounded-sm hover:bg-stone-100">
+      <div className="flex flex-row items-center justify-between space-y-1 px-4 py-3">
+        <div className="flex w-full flex-col justify-center gap-2 text-sm">
+          <h4 className="w-fit leading-none">{props.name}</h4>
+        </div>
+        <div className="flex w-2/3 flex-row items-center justify-start">
+          <div className="flex w-fit items-center gap-2">
+            <p className="rounded-md border border-gray-400 bg-stone-300 px-1 py-0.5 text-xs">
+              9 Tokens This Month
+            </p>
+          </div>
+        </div>
         <div className="flex flex-row items-center gap-4">
-          <p className="flex w-min flex-row items-center justify-center gap-2 rounded bg-stone-200 px-2 py-1 font-mono text-xs">
-            {props.id}
-            <button onClick={() => navigator.clipboard.writeText(props.id)}>
-              <Copy className="h-4 w-4 text-stone-900" />
-            </button>
-          </p>
-          <Button
-            className="bg-red-500 hover:bg-red-700"
-            onClick={() =>
-              deleteRootClient.mutate({
-                id: props.id,
-              })
-            }
-          >
-            {deleteRootClient.isLoading ? <Spinner /> : "Delete"}
-          </Button>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <button className="flex flex-row gap-2 rounded border border-stone-300 bg-white p-1 shadow-sm focus:outline-none focus:ring-0 focus:ring-transparent focus:ring-offset-0">
+                <Ellipses />
+              </button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              <DropdownMenuLabel>Actions</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <button
+                  className="flex w-full flex-row items-center gap-2 text-left font-light text-stone-900 focus:outline-none focus:ring-0 focus:ring-transparent focus:ring-offset-0"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    navigator.clipboard
+                      .writeText(props.id)
+                      .then(() => {
+                        toast({
+                          title: "Client ID Copied",
+                          description: "Client ID copied to clipboard",
+                        });
+                      })
+                      .catch((err) => {
+                        console.error(err);
+                        alert(err);
+                      });
+                  }}
+                >
+                  Copy ID
+                  <Copy className="h-3 w-3 text-stone-900" />
+                </button>
+              </DropdownMenuItem>
+              <DropdownMenuItem>
+                <button
+                  className="w-full text-left font-light text-red-700 focus:outline-none focus:ring-0 focus:ring-transparent focus:ring-offset-0"
+                  onClick={(e) => {
+                    e.stopPropagation();
+                    deleteRootClient.mutate({ id: props.id });
+                  }}
+                >
+                  Delete
+                </button>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
         </div>
       </div>
-      <Separator />
     </div>
   );
 }
@@ -67,9 +114,26 @@ export function RootClients(props: Props) {
           Create Root Client
         </Button>
       </div>
-      {props.data.map((client, idx) => (
-        <RootClientItem key={idx} {...client} />
-      ))}
+      {props.data.length === 0 ? (
+        <div className="flex flex-col items-center justify-center rounded-md">
+          <div className="flex flex-col items-center justify-between gap-4 p-4">
+            <h1 className="text-2xl font-semibold">No root clients found</h1>
+            <p className="text text-stone-500">
+              Looks like there are no root clients for this workspace. Create
+              one to get started.
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col rounded-md border border-stone-300 bg-stone-50 shadow">
+          {props.data.map((client, idx) => (
+            <div key={idx}>
+              <RootClientItem key={idx} {...client} />
+              {idx < props.data.length - 1 && <Separator />}
+            </div>
+          ))}
+        </div>
+      )}
     </>
   );
 }
