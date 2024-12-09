881795d8e0cf935329d40ebee1fb4dc222deff80
ethan-stone <ethan.stone9352@gmail.com>
2024-12-08 22:05:26 -0500

    add placeholder if no members

--- packages/web/src/app/dashboard/settings/team/page.tsx
+++ packages/web/src/app/dashboard/settings/team/page.tsx
@@ -31,17 +31,29 @@ export default async function TeamPage() {
         <h2 className="text-2xl font-semibold">Team</h2>
         {user.role === "admin" && <InviteMemberButton />}
       </div>
-      <div className="flex flex-col rounded-md border border-stone-300 bg-stone-50 shadow">
-        {members.map((member) => (
-          <MemberItem
-            key={member.id}
-            user={{
-              id: member.id,
-              email: member.user.email,
-            }}
-          />
-        ))}
-      </div>
+      {members.length === 0 ? (
+        <div className="flex flex-col items-center justify-center rounded-md">
+          <div className="flex flex-col items-center justify-between gap-4 p-4">
+            <h1 className="text-2xl font-semibold">No members found</h1>
+            <p className="text text-stone-500">
+              Looks like there are no members for this workspace. Invite a
+              member to get started.
+            </p>
+          </div>
+        </div>
+      ) : (
+        <div className="flex flex-col rounded-md border border-stone-300 bg-stone-50 shadow">
+          {members.map((member) => (
+            <MemberItem
+              key={member.id}
+              user={{
+                id: member.id,
+                email: member.user.email,
+              }}
+            />
+          ))}
+        </div>
+      )}
     </div>
   );
 }
